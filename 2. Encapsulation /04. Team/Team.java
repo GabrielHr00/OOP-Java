dc34d284e47d413295f07a4d6b41f9e03d4a8846
gabrielh00 <a11914741@univie.ac.at>
2021-10-30 15:23:54 +0200

    04. Lab

---
+++ 2. Encapsulation /04. Team/Team.java
@@ -0,0 +1,54 @@
+package task;
+
+import task.Person;
+
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+
+    public Team(String name) {
+        this.name = name;
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void addPlayer(Person person){
+        if(person.getAge() < 40){
+            this.firstTeam.add(person);
+        }
+        else{
+            this.reserveTeam.add(person);
+        }
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(this.firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(this.reserveTeam);
+    }
+
+    private void setFirstTeam(List<Person> firstTeam) {
+        this.firstTeam = firstTeam;
+    }
+
+    private void setReserveTeam(List<Person> reserveTeam) {
+        this.reserveTeam = reserveTeam;
+    }
+}

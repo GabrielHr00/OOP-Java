885e42619ac35aeff0de5beb13fff295faee55cd
gabrielh00 <a11914741@univie.ac.at>
2021-11-04 21:47:09 +0100

    01. Lab

---
+++ 3. Inheritance/01.SingleInheritance/Main.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}

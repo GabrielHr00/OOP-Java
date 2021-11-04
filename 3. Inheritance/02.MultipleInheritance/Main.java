ea1bb69ce0f5ca5e244958937b7f6c5891c1c76c
gabrielh00 <a11914741@univie.ac.at>
2021-11-04 21:48:19 +0100

    01. Lab

---
+++ 3. Inheritance/02.MultipleInheritance/Main.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+    }
+}

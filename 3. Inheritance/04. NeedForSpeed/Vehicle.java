26762f71226ea4325bd3ac777768b2687bea7cb3
gabrielh00 <a11914741@univie.ac.at>
2021-11-06 13:08:32 +0100

    04. Exercise

---
+++ 3. Inheritance/04. NeedForSpeed/Vehicle.java
@@ -0,0 +1,49 @@
+package needForSpeed;
+
+public class Vehicle {
+    private final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+    }
+
+    public void drive(double kilometers){
+        double consum = kilometers * fuelConsumption;
+        if(consum <= fuel){
+            this.fuel -= consum;
+        }
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}

f3e410b2e72f303011e411d329d4ed2146593281
gabrielh00 <a11914741@univie.ac.at>
2021-11-12 16:02:00 +0100

    03. Lab

---
+++ 5. Polymorphysm/03. Animals/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args){
+        Animal cat = new Cat("Oscar", "Whiskas");
+        Animal dog = new Dog("Rocky", "Meat");
+        System.out.println(cat.explainSelf());
+        System.out.println(dog.explainSelf());
+        
+    }
+}

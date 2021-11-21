3e7fc1fe699e157d5fb446548205b472b1190b19
gabrielh00 <a11914741@univie.ac.at>
2021-11-21 11:07:47 +0100

    4. Exercise

---
+++ 6. Abstraction/barracksWars_2/core/commands/Fight.java
@@ -0,0 +1,17 @@
+package barracksWars.core.commands;
+
+import barracksWars.interfaces.Executable;
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
+import jdk.jshell.spi.ExecutionControl;
+
+public class Fight extends Command implements Executable {
+    public Fight(String[] data, Repository repository, UnitFactory unitFactory) {
+        super(data, repository, unitFactory);
+    }
+
+    @Override
+    public String execute() throws ExecutionControl.NotImplementedException {
+        return "fight";
+    }
+}
